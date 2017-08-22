72571a9a428dc8f0b8b37f3ade5f7b808e8fd338
Suneel Dondapati <dsuneel1@gmail.com>
2017-08-23 00:52:38 +0530

    new file added

--- TestMart/src/io/github/Suneel123/business/ProductServiceImpl.java
+++ TestMart/src/io/github/Suneel123/business/ProductServiceImpl.java
@@ -1,16 +1,10 @@
-package io.github.Suneel123;
+package io.github.Suneel123.business;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.jws.WebMethod;
-import javax.jws.WebService;
-
-@WebService
-public class ProductCatalog {
+public class ProductServiceImpl {
 	
-	@WebMethod /*This is optional because when you give annotation @WebService it assumes all the
-	public methods are of web methods*/
 	public List<String> getProductCategories() {
 		List<String> categories = new ArrayList<String>();
 		categories.add("Books");
