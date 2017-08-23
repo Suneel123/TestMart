a4f05e411936f399ff168e0769c0dd4e085594d1
Suneel Dondapati <dsuneel1@gmail.com>
2017-08-24 01:37:49 +0530

    Improved the implementation by adding some methods in both the implementation class and main class

--- TestMart/src/io/github/Suneel123/ProductCatalog.java
+++ TestMart/src/io/github/Suneel123/ProductCatalog.java
@@ -1,21 +1,31 @@
 package io.github.Suneel123;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import io.github.Suneel123.business.ProductServiceImpl;
+
 @WebService
 public class ProductCatalog {
+	
+	ProductServiceImpl productService = new ProductServiceImpl();
 	
 	@WebMethod /*This is optional because when you give annotation @WebService it assumes all the
 	public methods are of web methods*/
 	public List<String> getProductCategories() {
-		List<String> categories = new ArrayList<String>();
-		categories.add("Books");
-		categories.add("Music");
-		categories.add("Movies");
-		return categories;
+		return productService.getProductCategories();
 	}
+	
+	@WebMethod
+	public List<String> getProducts(String category) {
+		return productService.getProducts(category);
+	}
+	
+	@WebMethod
+	public boolean addProduct(String category, String product) {
+		return productService.addProduct(category, product);
+	}
+	
 }
--- TestMart/src/io/github/Suneel123/business/ProductServiceImpl.java
+++ TestMart/src/io/github/Suneel123/business/ProductServiceImpl.java
@@ -5,6 +5,24 @@ import java.util.List;
 
 public class ProductServiceImpl {
 	
+	List<String> bookList = new ArrayList<String>();
+	List<String> musicList = new ArrayList<String>();
+	List<String> moviesList = new ArrayList<String>();
+	
+	public void productServiceImpl() {
+		bookList.add("Game of Thrones");
+		bookList.add("Alchemist");
+		bookList.add("Harry Porter");
+		
+		musicList.add("Dark Horse");
+		musicList.add("Work from Home");
+		musicList.add("Part of Me");
+		
+		moviesList.add("Drunkirk");
+		moviesList.add("Wonder Woman");
+		moviesList.add("X-men");
+	}
+	
 	public List<String> getProductCategories() {
 		List<String> categories = new ArrayList<String>();
 		categories.add("Books");
@@ -12,4 +30,34 @@ public class ProductServiceImpl {
 		categories.add("Movies");
 		return categories;
 	}
+	
+	public List<String> getProducts(String category) {
+		switch(category.toLowerCase()) {
+		case "books":
+			return bookList;
+		case "music":
+			return musicList;
+		case "movies":
+			return moviesList;
+		}
+		return null;
+	}
+	
+	public boolean addProduct(String category, String product) {
+		switch(category.toLowerCase()) {
+		case "books":
+			bookList.add(product);
+			break;
+		case "music":
+			musicList.add(product);
+			break;
+		case "movies":
+			moviesList.add(product);
+			break;
+		default:
+			return false;
+		}
+		return true;
+	}
+	
 }
